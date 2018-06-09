169bf1a5745039512d246d96e28c446a03b5092c
peter279k <peter279k@gmail.com>
2018-06-09 12:15:00 +0800

    Test enhancement

--- tests/RemoveStopWordsTest.php
+++ tests/RemoveStopWordsTest.php
@@ -9,14 +9,36 @@ use function Rap2hpoutre\RemoveStopWords\remove_stop_words;
  */
 class RemoveStopWordsTest extends TestCase
 {
-    public function testConversion()
+    public function conversionDataProvider()
     {
-        $this->assertEquals('Hello', remove_stop_words('Hello'));
-        $this->assertEquals(' plaît   majesté', remove_stop_words('Ça plaît à sa majesté', 'fr'));
-        $this->assertEquals(' quick brown fox jumps   lazy dog', remove_stop_words('The quick brown fox jumps over the lazy dog'));
-        $this->assertEquals('Portez  vieux whisky  juge blond  fume', remove_stop_words('Portez ce vieux whisky au juge blond qui fume', 'fr'));
-        $this->assertEquals(
-            '  must explain      mistaken idea  denouncing pleasure  praising pain  born    give   complete account   system,  expound  actual teachings   great explorer   truth,  master-builder  human happiness.', 
-            remove_stop_words('But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness.'));
+        return [
+            ['Hello', 'Hello'],
+            [' quick brown fox jumps   lazy dog', 'The quick brown fox jumps over the lazy dog'],
+            ['  must explain      mistaken idea  denouncing pleasure  praising pain  born    give   complete account   system,  expound  actual teachings   great explorer   truth,  master-builder  human happiness.', 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness.'],
+        ];
+    }
+
+    /**
+     * @dataProvider conversionDataProvider
+     */
+    public function testConversion($expectedString, $string)
+    {
+        $this->assertEquals($expectedString, remove_stop_words($string));
+    }
+
+    public function conversionWithLocaleDataProvider()
+    {
+        return [
+            [' plaît   majesté', 'Ça plaît à sa majesté', 'fr'],
+            ['Portez  vieux whisky  juge blond  fume', 'Portez ce vieux whisky au juge blond qui fume', 'fr'],
+        ];
+    }
+
+    /**
+     * @dataProvider conversionWithLocaleDataProvider
+     */
+    public function testConversionWithLocale($expectedString, $string, $locale)
+    {
+        $this->assertEquals($expectedString, remove_stop_words($string, $locale));
     }
 }
